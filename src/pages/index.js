fa404ff427664a7cc5edf76040a383611cdabe25
unknown <pedro.henrickcs@gmail.com>
2019-08-03 17:36:55 -0300

    include social midia

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Helmet from 'react-helmet';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
@@ -10,8 +11,29 @@ const Home = () => (
 		description="Front-end developer, developing web pages" 
 		keywords="React, Gatsby, Front-end, developer, API"
 		/>
+		<Helmet>
+			<script src="https://kit.fontawesome.com/1349f8977b.js"></script>
+		</Helmet>
 		<h1 className="title has-text-centered">Pedro Henrick Cavalcante Souza</h1>
 		<h2 className="subtitle has-text-centered">Web Developer</h2>
+		<div className="has-text-centered">
+			<a href="https://github.com/pedrohenrickcs" target="_blank" aria-label="Github">
+				<span className="icon is-large">
+					<i class="fab fa-2x fa-github-square">
+					</i>
+				</span>
+			</a>
+			<a href="https://www.linkedin.com/in/pedro-henrickcs/" target="_blank" aria-label="Linkedin">
+				<span className="icon is-large">
+					<i class="fab fa-2x fa-linkedin"></i>
+				</span>
+			</a>
+			<a href="https://codepen.io/pedrohenrickcs/" target="_blank" aria-label="Codepen">
+				<span className="icon is-large">
+					<i class="fab fa-2x fa-codepen"></i>
+				</span>
+			</a>
+		</div>
 
 		<section className="section content is-size-5-desktop is-size-6-touch">
 			<h4>Oi, eu sou o Pedro</h4>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
+import Helmet from 'react-helmet';
 
 const Projects = () => (
 	<Layout>
@@ -10,11 +11,16 @@ const Projects = () => (
             keywords="React, Gatsby, Front-end, developer, API"
         ></SEO>
 
+		<Helmet>
+			<script src="https://kit.fontawesome.com/1349f8977b.js"></script>
+		</Helmet>
+
 		<section className="section is-size-5-desktop is-size-6-touch">
-			<h1 className="title has-text-centered">Projects</h1>
+			<h1 className="title has-text-centered">
+				Projects</h1>
 			<hr />
 			<p className="has-text-centered">
-				Veja alguns de meus projetos no meu <a href="https://github.com/pedrohenrickcs" target="_blank">GitHub</a>
+				Veja alguns de meus projetos no meu <a href="https://github.com/pedrohenrickcs" target="_blank" aria-label="Github"><i class="fab fa-github-square"></i>&nbsp;GitHub</a>
 			</p>
 		</section>
 	</Layout>
